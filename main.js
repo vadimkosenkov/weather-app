0cf5837f776d61d642c2148a57d90ef34ee5045f
Vadim Kosenkov <vadimkosenkov.work@gmail.com>
2021-05-11 15:03:25 +0300

    add new DOM build by JS

--- main.js
+++ main.js
@@ -1,3 +1,72 @@
+//______________new DOM build______________
+const root = document.querySelector('#root')
+
+// ______________Header______________
+const header = document.createElement('header')
+header.classList.add('header')
+root.append(header)
+
+const headerSpanIntro = document.createElement('span')
+headerSpanIntro.classList.add('intro')
+headerSpanIntro.innerText = 'Weather-app by Vadim Kosenkov'
+header.append(headerSpanIntro)
+
+const headerTimeContainer = document.createElement('span')
+headerTimeContainer.classList.add('time_container')
+header.append(headerTimeContainer)
+
+const spanInTimeContainer = document.createElement('span')
+spanInTimeContainer.innerText = 'Время:'
+headerTimeContainer.append(spanInTimeContainer)
+
+const timeInTimeContainer = document.createElement('span')
+timeInTimeContainer.classList.add('time')
+headerTimeContainer.append(timeInTimeContainer)
+
+// ______________Main______________
+const main = document.createElement('main')
+main.classList.add('main')
+root.append(main)
+
+const mainH1 = document.createElement('h1')
+mainH1.innerText = 'Погода'
+main.append(mainH1)
+
+const mainDivCountry = document.createElement('div')
+mainDivCountry.classList.add('country')
+main.append(mainDivCountry)
+
+const mainDivTempBlock = document.createElement('div')
+mainDivTempBlock.classList.add('temp_block')
+main.append(mainDivTempBlock)
+
+const tempInTempBlock = document.createElement('span')
+tempInTempBlock.classList.add('temp')
+mainDivTempBlock.append(tempInTempBlock)
+
+const iconInTempBlock = document.createElement('img')
+iconInTempBlock.classList.add('icon')
+iconInTempBlock.setAttribute('src', ' ')
+mainDivTempBlock.append(iconInTempBlock)
+
+const descriptionInTempBlock = document.createElement('span')
+descriptionInTempBlock.classList.add('description')
+mainDivTempBlock.append(descriptionInTempBlock)
+
+const mainHumidity = document.createElement('div')
+mainHumidity.classList.add('humidity')
+main.append(mainHumidity)
+
+const mainWindSpeed = document.createElement('div')
+mainWindSpeed.classList.add('wind_speed')
+main.append(mainWindSpeed)
+
+// ______________Footer______________
+const footer = document.createElement('footer')
+root.append(footer)
+
+
+
 const time = document.querySelector('.time')
 const country = document.querySelector('.country')
 const temp = document.querySelector('.temp')
