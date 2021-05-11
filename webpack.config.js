d108d225044394c62a10dd9cd259d7fdb44d6e3c
Sergey Pismennyi <llshaxll@gmail.com>
2021-05-11 15:17:45 +0300

    file loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,45 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
+module.exports = (env, argv) => {
+  const isDev = argv.mode === "development";
 
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
+  return {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.(png|jpe?g|gif|woff|ttf|otf|eot|woff2)$/i,
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[ext]",
+          },
+        },
+      ],
+    },
+
+    devtool: isDev ? "inline-source-map" : false,
+    devServer: {
+      contentBase: "./dist",
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+      new MiniCssExtractPlugin(),
     ],
-  },
-
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: "./dist",
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: path.resolve(__dirname, "public", "index.html"),
-    }),
-    new MiniCssExtractPlugin(),
-  ],
+  };
 };
