b1d02eb817e76e855e233725db4e2fe5f7501984
Sergey Pismennyi <llshaxll@gmail.com>
2021-05-11 16:03:40 +0300

    eslint

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  trailingComma: 'es5',
+  semi: true,
+  eslintIntegration: true,
+  spaceBeforeFunctionParen: true,
+  endOfLine: 'auto',
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env, argv) => {
-  const isDev = argv.mode === "development";
+  const isDev = argv.mode === 'development';
 
   return {
-    mode: "development",
-    entry: "./src/index.js",
+    mode: 'development',
+    entry: './src/index.js',
     output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
       clean: true,
     },
 
@@ -18,27 +18,27 @@ module.exports = (env, argv) => {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
         {
           test: /\.(png|jpe?g|gif|woff|ttf|otf|eot|woff2)$/i,
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            name: "[path][name].[ext]",
+            name: '[path][name].[ext]',
           },
         },
       ],
     },
 
-    devtool: isDev ? "inline-source-map" : false,
+    devtool: isDev ? 'inline-source-map' : false,
     devServer: {
-      contentBase: "./dist",
+      contentBase: './dist',
     },
 
     plugins: [
       new HtmlWebpackPlugin({
-        filename: "index.html",
-        template: path.resolve(__dirname, "public", "index.html"),
+        filename: 'index.html',
+        template: path.resolve(__dirname, 'public', 'index.html'),
       }),
       new MiniCssExtractPlugin(),
     ],
