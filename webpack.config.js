3470c9ff3597f4fc8bc3f3514b477b47363cd61e
Sergey Pismennyi <llshaxll@gmail.com>
2021-05-11 15:08:30 +0300

    style loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,14 +10,26 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist",
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: path.resolve(__dirname, "public", "index.html"),
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
