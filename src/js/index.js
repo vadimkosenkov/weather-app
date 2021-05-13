1dde799aeaa17b37a276e093281d18194706967f
Vadim Kosenkov <vadimkosenkov.work@gmail.com>
2021-05-13 15:12:32 +0300

    fixed errors

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import '../styling/index.css';
-import { header } from './components/header/header.js';
-import { main } from './components/main/main.js';
-import { footer } from './components/footer/footer.js';
+import { header } from './components/header/header';
+import { main } from './components/main/main';
+import { footer } from './components/footer/footer';
 
 const root = document.querySelector('#root');
 root.append(header);
@@ -17,13 +17,10 @@ const humidity = document.querySelector('.humidity');
 const windSpeed = document.querySelector('.wind_speed');
 
 function timeValidation(hours, minutes) {
-    if (hours < 10) {
-        hours = '0' + hours;
-    }
-    if (minutes < 10) {
-        minutes = '0' + minutes;
-    }
-    time.innerText = `${hours}:${minutes}`;
+    const tmpHours = hours < 10 ? `0${hours}` : hours;
+    const tmpMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+    time.innerText = `${tmpHours}:${tmpMinutes}`;
 }
 
 setInterval(() => {
@@ -31,26 +28,26 @@ setInterval(() => {
     timeValidation(timeValue.getHours(), timeValue.getMinutes())
 }, 1000)
 
+
 function weather(_cityID) {
-    let cityID = _cityID,
-        apiUrl = 'https://api.openweathermap.org/data/2.5/',
-        apiKey = '444c2bfd114207e49fbfe7ecd26904d1',
-        apiQuery = apiUrl + '/weather?id=' + cityID + '&units=metric&lang=ru&appid=' + apiKey;
+    const cityID = _cityID;
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/';
+    const apiKey = '444c2bfd114207e49fbfe7ecd26904d1';
+    const apiQuery = `${apiUrl}/weather?id='${cityID}'&units=metric&lang=ru&appid='${apiKey}`
 
     fetch(apiQuery)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
             country.innerText = `${data.name}, ${data.sys.country}`;
-            temp.innerText = Math.round(data.main.temp) + '°C';
+            temp.innerText = `${Math.round(data.main.temp)}°C`;
             const newDescription = data.weather[0].description;
             const upperDescription = newDescription[0].toUpperCase() + newDescription.slice(1);
             description.innerText = upperDescription;
             humidity.innerText = `Относительная влажность: ${data.main.humidity}%`;
             windSpeed.innerText = `Скорость ветра: ${data.wind.speed}м/с`;
-            icon.setAttribute('src', 'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png');
+            icon.setAttribute('src', `https://openweathermap.org/img/w/${data.weather[0].icon}.png`);
         })
-        .catch(error => console.error('Ошибка получение погоды. Причина: ' + error));
+        .catch(error => console.error(`Ошибка получение погоды. Причина:${error}`));
 }
 
 const PLACES = [625144, 627907, 629634, 627904, 620127, 625665];
@@ -59,7 +56,8 @@ weather(PLACES[0]);
 let counter = 0;
 setTimeout(function updateWeather() {
     weather(PLACES[counter]);
-    counter === PLACES.length - 1 ? (counter = 0) : counter++;
+    counter = counter === PLACES.length - 1 ? 0 : counter + 1;
+
     setTimeout(updateWeather, 3000);
 }, 0);
 
@@ -71,4 +69,5 @@ setTimeout(function updateWeather() {
 // }
 
 // const weatherWidget = new WheatherWidget
+// weatherWidget.getWeather()// const weatherWidget = new WheatherWidget
 // weatherWidget.getWeather()
