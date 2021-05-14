f40586b4002e56dc1cf60709745345662606d3de
Vadim Kosenkov <vadimkosenkov.work@gmail.com>
2021-05-14 14:50:31 +0300

    add methods to func.prototype (create_elements.js)

--- src/js/components/creater_elements/create_elements.js
+++ src/js/components/creater_elements/create_elements.js
@@ -1,22 +1,22 @@
-function CreaterElements() {
-    this.createElem = function(tag) {
-        this.elem = document.createElement(tag)
-        return this
-    }
-    this.createClass = function(className) {
-        this.elem.classList.add(className)
-        return this
-    }
-    this.createText = function(text) {
-        this.elem.innerText = text
-        return this
-    }
-    this.createAttr = function(attr, value) {
-        this.elem.setAttribute(attr, value)
-        return this
-    }
-    this.createAppend = function(parent) {
-        document.querySelector(parent).append(this.elem)
-    }
+function CreaterElements() {}
+CreaterElements.prototype.createElem = function(tag) {
+    this.elem = document.createElement(tag)
+    return this
 }
+CreaterElements.prototype.createClass = function(className) {
+    this.elem.classList.add(className)
+    return this
+}
+CreaterElements.prototype.createText = function(text) {
+    this.elem.innerText = text
+    return this
+}
+CreaterElements.prototype.createAttr = function(attr, value) {
+    this.elem.setAttribute(attr, value)
+    return this
+}
+CreaterElements.prototype.createAppend = function(parent) {
+    document.querySelector(parent).append(this.elem)
+}
+
 export { CreaterElements }
