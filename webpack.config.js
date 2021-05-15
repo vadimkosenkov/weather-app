7831ff650dd907a6029e1d3f3bfb33a64cfc0dd8
Sergey Pismennyi <llshaxll@gmail.com>
2021-05-15 16:27:19 +0300

    minimize css

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,58 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = (env, argv) => {
-    const isDev = argv.mode === 'development';
+  const isDev = argv.mode === 'development';
 
-    return {
-        mode: 'development',
-        entry: './src/js/index.js',
-        output: {
-            filename: '[name].bundle.js',
-            path: path.resolve(__dirname, 'dist'),
-            clean: true,
+  return {
+    mode: 'development',
+    entry: './src/js/index.js',
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
         },
-
-        module: {
-            rules: [{
-                    test: /\.css$/i,
-                    use: [MiniCssExtractPlugin.loader, 'css-loader'],
-                },
-                {
-                    test: /\.(png|jpe?g|gif|woff|ttf|otf|eot|woff2)$/i,
-                    loader: 'file-loader',
-                    options: {
-                        name: '[path][name].[ext]',
-                    },
-                },
-            ],
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
-
-        devtool: isDev ? 'inline-source-map' : false,
-        devServer: {
-            contentBase: path.resolve(__dirname, 'dist'),
+        {
+          test: /\.(png|jpe?g|gif|woff|ttf|otf|eot|woff2)$/i,
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          },
         },
+      ],
+    },
 
-        plugins: [
-            new HtmlWebpackPlugin({
-                filename: 'index.html',
-                template: path.resolve(__dirname, 'public', 'index.html'),
-            }),
-            new MiniCssExtractPlugin(),
-        ],
-    };
+    optimization: {
+      minimizer: [new CssMinimizerPlugin(), '...'],
+    },
+
+    devtool: isDev ? 'inline-source-map' : false,
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: path.resolve(__dirname, 'public', 'index.html'),
+      }),
+      new MiniCssExtractPlugin(),
+    ],
+  };
 };
